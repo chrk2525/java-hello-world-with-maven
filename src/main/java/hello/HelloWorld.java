788cee100b0fb4437ff143d011dcb319114f9c01
ramakrishna <chramakrishna2525@gmail.com>
2021-04-19 08:46:44 +0530

    changing files

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("Now the time in Hyderabad is: " + currentTime);
+		  System.out.println("Now the time in Nakrekal is: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
