c26ac47abade610d8d78f83a44d46e93ba36bff7
ramakrishna <chramakrishna2525@gmail.com>
2021-04-20 09:38:08 +0530

    file changes

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Hello Nakrekal!!!";
+        return "Hello Nari how r u";
     }
 }
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("Now the time in Nakrekal is: " + currentTime);
+		  System.out.println("hello world wt is ur time: " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
