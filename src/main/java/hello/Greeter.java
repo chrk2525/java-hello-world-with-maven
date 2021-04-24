cbb219b3dd56b175a29bf4cff2376483960754c2
ramakrishna <chramakrishna2525@gmail.com>
2021-04-24 14:01:54 +0530

    hi

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Hello Ravi Kiran! How are you???";
+        return "Hello take care of your lappyh u???";
     }
 }
--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
     public static void main(String[] args) {
       LocalTime currentTime = new LocalTime();
-		  System.out.println("Hello Hyderabad!!!, the current time in hyderabad is " + currentTime);
+		  System.out.println("Hi world welcome to please be safe from corona " + currentTime);
 
         Greeter greeter = new Greeter();
         System.out.println(greeter.sayHello());
